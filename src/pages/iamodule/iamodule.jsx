1f3e85d3c64cc1fd7db76955b54843595a13ec0a
KennethTaipe06 <kataipeg@uce.edu.ec>
2025-01-21 15:29:30 -0500

    feat: actualizar estilos y nombres en el módulo de IA y navbar

--- src/pages/iamodule/iamodule.jsx
+++ src/pages/iamodule/iamodule.jsx
@@ -45,39 +45,29 @@ const Iamodule = () => {
       </section>
       <div className="flex flex-col lg:flex-row h-[89vh] bg-white text-black">
         {/* Sidebar */}
-        <aside className="w-full lg:w-64 bg-gray-800 p-4 text-white lg:block hidden">
+        <aside className="w-full lg:w-64 bg-black p-4 text-white lg:block hidden">
           <h1 className="text-2xl font-bold mb-6">AI Module</h1>
-          <div className="relative mb-6">
-            <input
-              type="text"
-              placeholder="Buscar en Marketplace"
-              className="w-full p-2 pl-10 rounded-full bg-gray-700 text-white focus:outline-none"
-            />
-          </div>
+        
 
-          <button
-            className="w-full mt-4 py-2 bg-gradient-to-r from-gray-300 to-gray-500 text-black rounded hover:from-gray-400 hover:to-gray-600"
-            onClick={() => navigate('/marketplace/create-product')}
-          >
-            ChatBot
+          <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-white-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 w-full h-12">
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-black dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 w-full text-center">
+              ChatBot
+            </span>
           </button>
-          <button
-            className="w-full mt-4 py-2 bg-gradient-to-r from-gray-300 to-gray-500 text-black rounded hover:from-gray-400 hover:to-gray-600"
-            onClick={() => navigate('/marketplace/create-product')}
-          >
-            Conceptual Map Generator
+          <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-white-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800 w-full h-12">
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-black dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 w-full text-center">
+              Conceptual Map Generator
+            </span>
           </button>
-          <button
-            className="w-full mt-4 py-2 bg-gradient-to-r from-gray-300 to-gray-500 text-black rounded hover:from-gray-400 hover:to-gray-600"
-            onClick={() => navigate('/marketplace/create-product')}
-          >
-            Summarize
+          <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-white-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 w-full h-12">
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-black dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 w-full text-center">
+              Summarize
+            </span>
           </button>
-          <button
-            className="w-full mt-4 py-2 bg-gradient-to-r from-gray-300 to-gray-500 text-black rounded hover:from-gray-400 hover:to-gray-600"
-            onClick={() => navigate('/marketplace/create-product')}
-          >
-            Image Generator
+          <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-white-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 w-full h-12">
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-black dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 w-full text-center">
+              Image Generator
+            </span>
           </button>
         </aside>
 
@@ -111,7 +101,11 @@ const Iamodule = () => {
                     onChange={(e) => setMessage(e.target.value)}
                     className="flex-grow mr-2"
                   />
-                  <Button onClick={handleSendMessage}>Enviar</Button>
+                  <button onClick={handleSendMessage} className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-white rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 w-1/5 h-12">
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-black dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 w-full text-center">
+              Send
+            </span>
+          </button>
                 </div>
               </CardBody>
             </Card>
--- src/widgets/layout/navbar.jsx
+++ src/widgets/layout/navbar.jsx
@@ -124,7 +124,7 @@ export function Navbar({ brandName, routes, action }) {
 }
 
 Navbar.defaultProps = {
-  brandName: "Material Tailwind React",
+  brandName: "EduPlace",
   action: (
     <a
       href="https://www.creative-tim.com/product/material-tailwind-kit-react"
